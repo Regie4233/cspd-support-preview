82543ca9488aed8197325e48909b92382fd28a2b
Regie4233 <regie4233@ymail.com>
2022-08-18 21:21:48 -0400

    refractoring room GET logic

--- src/Component/HomePage.js
+++ src/Component/HomePage.js
@@ -2,13 +2,13 @@ import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
-function HomePage(){
+function HomePage() {
     return (
         <>
-            
-           <h2 className='mainForm trayLocation'>This page is under developement</h2>
-           <h1 className='check'> deployed 0.3</h1>
-
+            <di className='HeaderL'>
+                <h2 className='mainForm trayLocation'>This page is under developement</h2>
+                <h1 className='check'> deployed 0.3</h1>
+            </di>
         </>
     );
 };
